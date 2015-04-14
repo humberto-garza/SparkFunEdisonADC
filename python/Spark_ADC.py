b321e4bf0d0f152b56f8b9499705ecf62cccb3dd
Jose Humberto Garza Rosado <humberto.garza.13@gmail.com>
2015-04-14 11:01:59 -0500

    Update Spark_ADC.py

--- python/Spark_ADC.py
+++ python/Spark_ADC.py
@@ -22,14 +22,14 @@ class Adc:
 	def adc_read(self):
 		os.system(self.command)
 		adc = os.popen("i2cget -y 1 0x48 0 w")
-    	value = adc.read()
-    	concat = value[0]+value[1]+value[3]+value[4]+value[5]+value[2]
-    	x = int(concat,16)
-    	os.close
-    	if x > 0x7FF:
-    		return -1*((0x7FF*2+2)+(~x))
-    	else:
-    		return x
+    		value = adc.read()
+    		concat = value[0]+value[1]+value[3]+value[4]+value[5]+value[2]
+    		x = int(concat,16)
+    		os.close
+    		if x > 0x7FF:
+    			return -1*((0x7FF*2+2)+(~x))
+    		else:
+    			return x
 
 	def __del__(self):
 		class_name = self.__class__.__name__
