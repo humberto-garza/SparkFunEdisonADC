cf55ee0a84ebc74d3d7dbf8759f9acb2ed390e49
Jose Humberto Garza Rosado <humberto.garza.13@gmail.com>
2015-06-25 16:40:14 -0500

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -52,15 +52,15 @@ using namespace std;
 //////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-	int ain1_operational_status = 							0b0;
-	int ain1_input_multiplexer_configuration = 				0b000;
+	int ain1_operational_status = 				0b0;
+	int ain1_input_multiplexer_configuration = 		0b000;
 	int ain1_programmable_gain_amplifier_configuration =	0b010;
-	int ain1_device_operating_mode =						0b0;
-	int ain1_date_rate =									0b100;
-	int ain1_comparator_mode = 								0b0;
-	int ain1_compulator_polarity = 							0b0;
-	int ain1_latching_comparator	=						0b0;
-	int ain1_comparator_queue_and_disable =					0b11;
+	int ain1_device_operating_mode =			0b0;
+	int ain1_date_rate =					0b100;
+	int ain1_comparator_mode =				0b0;
+	int ain1_compulator_polarity = 				0b0;
+	int ain1_latching_comparator	=			0b0;
+	int ain1_comparator_queue_and_disable =			0b11;
 	
 	//Adc ain1(bus, address);
 	Adc ain1(1, 0x48);
