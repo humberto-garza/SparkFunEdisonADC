36e36355d93b49e0831ffa976bf78e0c6a4f10a9
humberto-garza <humberto.garza.13@gmail.com>
2015-06-26 10:01:33 -0500

    Configuration Bits

--- Python/Spark_ADC.py
+++ Python/Spark_ADC.py
@@ -25,6 +25,86 @@
 
 # Created by Jose Humberto Garza Rosado
 
+# ///////////////////////////////////////////////////////////////////
+# //Set up the ADC register by sending the next bits:
+# //*Represents the suggested configuration to only read from A1N0
+
+# //int os:       Operational status/single-shot conversion start.
+# //          This bit determines the operational status of the device.
+# //          This bit can only be written when in power-down mode.
+# //          Write status:
+# //            0b0: No Effect.
+# //            0b1: Begin single conversion (when in power-down mode).* 
+# //          Read status
+# //            0b0: Device is currently performing a conversion.
+# //            0b1: Device is not currently performing a conversion.
+
+# //int imc:      Input multiplexer configuration.
+# //          These bits configure the input multiplexer.
+# //          VIN (AINX minus AINY):
+# //            0b000: AIN0 - AIN1
+# //            0b001: AIN0 - AIN3
+# //            0b010: AIN1 - AIN3
+# //            0b011: AIN2 - AIN3
+# //            0b100: AIN0 - GND*  
+# //            0b101: AIN1 - GND
+# //            0b110: AIN2 - GND
+# //            0b111: AIN3 - GND
+
+# //int pga:      Programmable gain amplifier configuration
+# //          These bits configure the programmable gain amplifier. 
+# //          FS:
+# //            0b000: +-6.144V
+# //            0b001: +-4.096V*
+# //            0b010: +-2.048V
+# //            0b011: +-1.024V
+# //            0b100: +-0.512V
+# //            0b101: +-0.256V
+# //            0b110: +-0.256V
+# //            0b111: +-0.256V
+
+# //int mode:     Device operating mode
+# //          This bit controls the current operational mode: 
+# //            0b0: Continuous conversion mode.*
+# //            0b1: Power-Down single-shot mode.
+
+# //int rate:     Data rate
+# //          These bits control the data rate setting:
+# //            0b000: 128SPS
+# //            0b001: 250SPS
+# //            0b010: 490SPS
+# //            0b011: 920SPS
+# //            0b100: 1600SPS*
+# //            0b101: 2400SPS
+# //            0b110: 3300SPS
+# //            0b111: 3300SPS
+
+# //int comp_mode:  Comparator mode
+# //          This bit controls the comparator mode of operation.
+# //          It changes whether the comparator is implemented as a traditional comparator 
+# //          or as a window comparator:
+# //            0b0: Traditional comparator with hysteresis.*
+# //            0b1: Window comparator.
+
+# //int comp_pol:   Comparator polarity
+# //          This but controls the polarity of the ALERT/RDY pin:
+# //            0b0: Comparator output is active low.*
+# //            0b1: ALERT/RDY pin is active high.
+
+# //int comp_lat:   Latching comparator
+# //          This bit controls whether the ALERT/RDY pin latches once asserted or clears once
+# //          conversions are within the margin of the upper and lower threshold values:
+# //            0b0: The comparator output is active low.*
+# //            0b1: ALERT/RDY pin is active high.            
+
+# //int comp_que:   Comparator queue and disable 
+# //          These bits perform two functions:
+# //            0b11: Disable the comparator function and put the ALERT/RDY pin into a high state.*
+# //              Otherwise they control the number of successive conversions exceeding the upper or
+# //              lower thresholds required before asserting the ALERT/RDY pin.
+# //            0b00: Assert after one conversion.
+# //            0b01: Assert after two conversions.
+# //            0b10: Assert after four conversions.
 
 import fcntl, os
 
